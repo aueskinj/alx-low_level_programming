72521c2194dda9b2450a2744b1a42eba3c01d404
aueskinj <71489113+aueskinj@users.noreply.github.com>
2022-09-15 12:20:39 +0300

    fix

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -2,8 +2,13 @@
 /**
  * main - entry point
  */ 
-int main()
+int main(void)
 {
-    int _isupper(int c);
-    return 0;
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
