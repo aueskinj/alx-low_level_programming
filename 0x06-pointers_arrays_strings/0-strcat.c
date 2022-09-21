8460e5e465a2b3237a41c75141d74c2ae49e4f04
aueskinj <mailto:aueskinj@gmail.com>
2022-09-20 22:05:23 -0700

    first push, string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+char *_strcat(char *dest, char *src)
+{
+	size_t  = strlen(dest);
+	n = strlen(src)
+       for (i=0; i<n && src[i] != '\0' ; i++
+		dest[dest_len + i] = src[i];
+ 	dest[dest_len + + i] = src[i];
+	
+	return 	dest;
+}	
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+more on pointers and string 
+0. function on string concatenation
