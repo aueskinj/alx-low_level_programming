f70fe0984a3fa64be2905a57fc8b2ad2fa298edc
aueskinj <71489113+aueskinj@users.noreply.github.com>
2022-09-22 09:10:20 +0300

    fixing betty issue one spaces

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,8 +12,8 @@ char *_strcat(char *dest, char *src)
 {
 	int dest_len  = strlen(dest);
 	int n = strlen(src);
-       for (int i=0; i<n && src[i] != '\0' ; i++)
+    for (int i=0; i<n && src[i] != '\0' ; i++)
 		dest[dest_len + i] = src[i];	
-	return 	dest;
+	return 	(dest);
 }
 
