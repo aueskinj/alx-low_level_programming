71d7d2bf0774e9ba7a246f6aa67325e414d4fc86
aueskinj <71489113+aueskinj@users.noreply.github.com>
2022-09-22 09:06:13 +0300

    fixed betty issue one description

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int dest_len  = strlen(dest);
