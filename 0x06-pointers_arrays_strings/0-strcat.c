97992b8e4b12f8b322fcfeff20bb7f87105d576c
aueskinj <71489113+aueskinj@users.noreply.github.com>
2022-09-22 09:00:38 +0300

    task 0, correct output, check betty linter

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,12 @@
 #include "main.h"
-#Include <string.h>
+#include <string.h>
 
 char *_strcat(char *dest, char *src)
 {
-	dest_len  = strlen(dest);
-	n = strlen(src)
-       for (i=0; i<n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = src[i];
- 	dest[dest_len + + i] = src[i];
-	
+	int dest_len  = strlen(dest);
+	int n = strlen(src);
+       for (int i=0; i<n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];	
 	return 	dest;
 }
 
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,18 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 
-char *string_toupper(char *str)
-{
-    int i;
-
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] - 32;
+char *cap_string(char *str)
+{  
+    
+    for(int i = 0; str[i] =! '\0'; i++)
+       {
+            if((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122))
+            {
+                if (i == 0 || str[i] == '\0')
+                {
+                    if(str[i] >=97 && str[i] <= 122)
+                    {
+                        str[i] = str[i] - 32;
+                    }
+                }
+                else 
+                {
+                    if (str[i] >= 65 && str[i] <=90)
+                    {
+                        str[i]=str[i] + 32;
+                    }
+                }
+            }
         }
-    }
-
-    return (str);
+    return str;
 }
     
