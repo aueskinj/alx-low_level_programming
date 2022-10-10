edd926fd53fa2e13764f8d43fb2da76d54019672
kimuhuNjuguna <71489113+aueskinj@users.noreply.github.com>
2022-10-10 11:08:44 +0300

    used ascii characters to improve my code.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,26 @@
-
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return:0
+ * main - main block
+ * Description: prints decimals upto 9 from 0
+ * Return: 0
  */
+
 int main(void)
 {
-	int i, y;
-	for (i = 0; i < 10; i++)
-	{
-		putchar (i + '0');
-	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-		putchar (y);
-	}
+        int num = 48, alph = 97;
 
-putchar('\n');
-return (0);
+        while (num <58)
+        {
+                putchar(num);
+                num++;
+        }
+        while (alph < 103)
+        {
+                putchar(alph);
+                alph++;
+        }
+
+        putchar('\n');
+        return (0);
 }
